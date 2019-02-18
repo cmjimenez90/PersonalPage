653fb8b14cff59fdc6837eabb17c429ceac53d00
Carlos M Jimenez <c.m.jimenez90@gmail.com>
2019-02-17 22:27:31 -0500

    add heading top project listing section

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <v-layout row wrap justify-center>
       <BioCard />
     </v-layout>
-    <v-layout row wrap justify-center class="mt-5">
+    <v-layout row wrap justify-center>
+      <v-flex xs12 class="text-xs-center ma-3">
+        <div class="subheading">Recent Projects</div>
+      </v-flex>
       <project-highlights :projects='projects' />
     </v-layout>
   </v-container>
