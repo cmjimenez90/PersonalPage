f6051a6a1bdfd021c053213c111bda77e602b3ca
Carlos M Jimenez <c.m.jimenez90@gmail.com>
2019-02-18 20:01:57 -0500

    redesign footer for better mobile display

--- components/main/Footer.vue
+++ components/main/Footer.vue
@@ -1,33 +1,39 @@
 <template>
-  <v-footer fixed>
-    <v-layout row wrap align-center justify-end>
-      <v-flex>
-        <div class="title">Developed Using: </div>
-      </v-flex>
-      <v-flex>
-        <v-btn block flat :href=vueifityLink >
-          <v-avatar size="25">
-            <v-img contain src='/v.png'/>
-          </v-avatar>
-          <span>Vuetify</span>
-        </v-btn>
-      </v-flex>
-      <v-flex>
-        <v-btn block flat :href=vueLink >
-          <v-avatar size="25">
-            <v-img contain src='/vue.png'/>
-          </v-avatar>
-          <span>Vue</span>
-        </v-btn>
-      </v-flex>
-      <v-flex>
-        <v-btn block flat :href=butterCMSLink>
-          <v-avatar size="130">
-            <v-img :src=butterCMSLogo contain/>
-          </v-avatar>
-        </v-btn>
-      </v-flex>
-    </v-layout>
+  <v-footer class='mt-2' height="auto">
+      <v-layout
+       wrap
+       class="text-xs-center"
+       justify-space-around
+       align-center
+      >
+        <v-flex
+         xs12
+         md6
+         class="text-md-right text-xs-center"
+        >
+          <div class="title">
+            Made With: 
+          </div>
+        </v-flex>
+        <v-flex
+         xs12
+         sm6
+         md2
+         v-for='tool in tools'
+         :key="tool.logo"
+        >
+          <v-btn
+           :href="tool.link"
+           flat
+          >
+            <v-img
+             :src='tool.logo'
+             contain
+             max-height="25"
+            />
+          </v-btn>
+        </v-flex>
+      </v-layout>
   </v-footer>
 </template>
 
@@ -35,10 +41,20 @@
 export default {
   data: function() {
     return {
-      vueifityLink: 'https://vuetifyjs.com/en/',
-      vueLink: 'https://vuejs.org/',
-      butterCMSLogo: 'https://cdn.buttercms.com/JSSDbrHPSnGlLUcyHTn5',
-      butterCMSLink: 'https://buttercms.com/'
+      tools: [
+        {
+          link: 'https://vuetifyjs.com/en/',
+          logo: '/v.png'
+        },
+        {
+          link: 'https://vuejs.org/',
+          logo: '/Vue.png'
+        },
+        {
+          link: 'https://buttercms.com/',
+          logo: 'https://cdn.buttercms.com/PGJPyIwaQ2KnOA8UyKfH'
+        }
+      ]
     }
   }
 }
