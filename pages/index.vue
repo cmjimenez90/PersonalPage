74abb4357d2ae5d3217f1cdca3e35dafa914b504
Carlos Jimenez <developer@cmjimenez.com>
2019-02-27 17:50:09 -0500

    adjust typos in description

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@ export default {
       name: 'Carlos Jimenez',
       caption: 'Problem Solver, Developer, Computer Technician',
       description:
-        'A new developer with a strong desire to take on any challenge. Carlos is currently employed as a Technical Support Technician for Rowan College at Gloucester County while also pursuing a degree in Computer Science. In the meantime, he continues to push him self by learning new technologies and creating different tools on the side; whether its to solve a problem at work or just for fun.'
+        'A new developer with a strong desire to take on any challenge. Carlos is currently employed as a Technical Support Technician for Rowan College at Gloucester County while also pursuing a degree in Computer Science. In the meantime, he continues to push himself by learning new technologies and creating different tools on the side; whether it is to solve a problem at work or just for fun!'
     }
   },
   async fetch({ store }) {
