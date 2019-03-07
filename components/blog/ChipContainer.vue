7137efb871d1f983ddbb65b820ce2261ec12ca7f
Carlos M Jimenez <developer@cmjimenez.com>
2019-03-06 21:19:24 -0500

    wrap heading and chips in vflexs

--- components/blog/ChipContainer.vue
+++ components/blog/ChipContainer.vue
@@ -1,26 +1,32 @@
 <template>
   <v-container>
-    <span>{{ heading }}</span>
-    <template v-if="reduceItemsShown">
-      <v-chip v-for="n in 3" :key="n" small label>
-        {{ items[n].name }}
-      </v-chip>
-      <v-tooltip bottom>
-        <span slot="activator">
-          <v-icon>more_horiz</v-icon>
-        </span>
-        <span>
-          <v-chip v-for="(item,index) in tooltipItems" :key="index" small label>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <span>{{ heading }}</span>
+      </v-flex>
+      <v-flex>
+        <template v-if="reduceItemsShown">
+          <v-chip v-for="n in 3" :key="n" small label>
+            {{ items[n].name }}
+          </v-chip>
+          <v-tooltip bottom>
+            <span slot="activator">
+              <v-icon>more_horiz</v-icon>
+            </span>
+            <span>
+              <v-chip v-for="(item,index) in tooltipItems" :key="index" small label>
+                {{ item.name }}
+              </v-chip>
+            </span>
+          </v-tooltip>
+        </template>
+        <template v-else>
+          <v-chip v-for="(item,index) in items" :key="index" small label>
             {{ item.name }}
           </v-chip>
-        </span>
-      </v-tooltip>
-    </template>
-    <template v-else>
-      <v-chip v-for="(item,index) in items" :key="index" small label>
-        {{ item.name }}
-      </v-chip>
-    </template>
+        </template>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -35,7 +41,7 @@ export default {
     },
     heading: {
       type: String,
-      default: 'HEAD HERE'
+      default: 'HEADING HERE'
     }
   },
   computed: {
