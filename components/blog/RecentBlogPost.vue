1896c4c5970867599ce17f652a7993adcfe3998e
Carlos M Jimenez <developer@cmjimenez.com>
2019-03-10 16:58:13 -0400

    restructure componenet layout center content within card.

--- components/blog/RecentBlogPost.vue
+++ components/blog/RecentBlogPost.vue
@@ -1,21 +1,26 @@
 <template>
-  <v-layout v-if="!carousel" wrap>
-    <v-flex v-for="(post,index) in sortedBlogPost" :key="index">
-      <BlogSummaryCard :blog-post="post" />
-    </v-flex>
-  </v-layout>
-  <v-layout v-else>
-    <v-flex>
-      <v-card>
-        <v-card-title
-          class="primary
+  <v-card>
+    <v-card-title
+      class="primary
           darken-2
           white--text
-          title"
-          primary-title
-        >
-          RECENT POST
-        </v-card-title>
+          title
+          "
+      primary-title
+    >
+      RECENT POST
+    </v-card-title>
+    <v-layout v-if="!carousel">
+      <v-container fluid>
+        <v-layout wrap>
+          <v-flex v-for="(post,index) in sortedBlogPost" :key="index">
+            <BlogSummaryCard :blog-post="post" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-layout>
+    <v-layout v-else>
+      <v-flex>
         <v-container>
           <v-carousel hide-delimiters light>
             <v-carousel-item v-for="(post,index) in sortedBlogPost" :key="index">
@@ -23,9 +28,9 @@
             </v-carousel-item>
           </v-carousel>
         </v-container>
-      </v-card>
-    </v-flex>
-  </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 <script>
 import BlogSummaryCard from '~/components/blog/BlogSummaryCard.vue'
