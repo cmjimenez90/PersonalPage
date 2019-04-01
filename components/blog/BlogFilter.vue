b9f2499f42bd6787a61f32098991e1cd788791c7
Carlos Jimenez <developer@cmjimenez.com>
2019-04-01 18:10:30 -0400

    Implement base filter gui functionality.
    todo: add ability to keep alphabetised.

--- components/blog/BlogFilter.vue
+++ components/blog/BlogFilter.vue
@@ -9,30 +9,56 @@
       <v-toolbar-title>Filter</v-toolbar-title>
     </v-toolbar>
     <v-card-text>
-      <v-list subheader>
-        <v-subheader inset>
+      <v-subheader>
+        Filter
+      </v-subheader>
+      <v-combobox
+        v-model="filter"
+        clearable
+        deleteable-chips
+        chips
+        multiple
+        append-icon=""
+        @click:clear="onReset()"
+      >
+        <template v-slot:selection="data">
+          <v-chip
+            :selected="data.selected"
+            close
+            small
+            @input="onRemove(data.item)"
+          >
+            {{ data.item.filterParameter.name }}
+          </v-chip>
+        </template>
+      </v-combobox>
+      <v-list>
+        <v-subheader>
           Categories
         </v-subheader>
         <v-list-tile
-          v-for="category in blogCategories"
+          v-for="(category, index) in blogCategories"
           :key="category.slug"
         >
           <v-list-tile-content>
-            <v-chip small>
+            <v-chip small @click="onFilter(index,'category')">
               {{ category.name }}
             </v-chip>
           </v-list-tile-content>
         </v-list-tile>
         <v-divider />
-        <v-subheader inset>
+        <v-subheader>
           Tags
         </v-subheader>
         <v-list-tile
-          v-for="tag in blogTags"
+          v-for="(tag,index) in blogTags"
           :key="tag.slug"
         >
           <v-list-tile-content>
-            <v-chip small>
+            <v-chip
+              small
+              @click="onFilter(index,'tag')"
+            >
               {{ tag.name }}
             </v-chip>
           </v-list-tile-content>
@@ -44,12 +70,53 @@
 
 <script>
 export default {
+  data: function() {
+    return {
+      filter: []
+    }
+  },
   computed: {
     blogCategories: function() {
-      return this.$store.state.blog.categories
+      return this.$store.state.blog.categories.slice()
     },
     blogTags: function() {
-      return this.$store.state.blog.tags
+      return this.$store.state.blog.tags.slice()
+    }
+  },
+  methods: {
+    onFilter(index, type) {
+      if (type === 'category') {
+        this.filter.push({
+          filterParameter: this.blogCategories[index],
+          type: type
+        })
+
+        this.blogCategories.splice(index, 1)
+      } else {
+        this.filter.push({
+          filterParameter: this.blogTags[index],
+          type: type
+        })
+        this.blogTags.splice(index, 1)
+      }
+    },
+    onRemove(item) {
+      this.filter.splice(this.filter.indexOf(item), 1)
+      if (item.type === 'category') {
+        this.blogCategories.push(item.filterParameter)
+      } else {
+        this.blogTags.push(item.filterParameter)
+      }
+    },
+    onReset() {
+      this.filter.forEach(filterItem => {
+        if (filterItem.type === 'category') {
+          this.blogCategories.push(filterItem.filterParameter)
+        } else {
+          this.blogTags.push(filterItem.filterParameter)
+        }
+      })
+      this.filter.splice(0, this.filter.length)
     }
   }
 }
