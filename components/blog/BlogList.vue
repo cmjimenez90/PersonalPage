fb6f5382377b6b672784f4ebd1d9126887508c00
Carlos Jimenez <developer@cmjimenez.com>
2019-04-07 20:41:47 -0400

    implement filter function for list of blog post

--- components/blog/BlogList.vue
+++ components/blog/BlogList.vue
@@ -12,7 +12,7 @@
         </v-toolbar-title>
       </v-toolbar>
     </v-flex>
-    <v-flex v-for="post in blogPostList" :key="post.name" class="py-2">
+    <v-flex v-for="post in filteredBlogs" :key="post.slug" class="py-2">
       <BlogSummaryCard :blog-post="post" />
     </v-flex>
   </v-layout>
@@ -32,9 +32,47 @@ export default {
       }
     }
   },
-  data: function() {
-    return {
-      blogPostList: this.blogList
+  computed: {
+    filteredBlogs: function() {
+      const filterCategories = []
+      const filterTags = []
+      this.$store.getters['blogFilter/getFilterParams'].forEach(filter => {
+        if (filter.type === 'category') {
+          filterCategories.push(filter.name)
+        } else {
+          filterTags.push(filter.name)
+        }
+      })
+      if (filterCategories.length < 1 && filterTags.length < 1) {
+        return this.blogList
+      } else {
+        return this.blogList.filter(blog => {
+          let matchesFilter = true
+          if (filterCategories.length >= 1) {
+            const categories = blog.categories.map(category => {
+              return category.name
+            })
+            for (let index = 0; index < filterCategories.length; index++) {
+              if (!categories.includes(filterCategories[index])) {
+                matchesFilter = false
+                break
+              }
+            }
+          }
+          if (filterTags.length >= 1 && matchesFilter === true) {
+            const tags = blog.tags.map(tag => {
+              return tag.name
+            })
+            for (let index = 0; index < filterTags.length; index++) {
+              if (!tags.includes(filterTags[index])) {
+                matchesFilter = false
+                break
+              }
+            }
+          }
+          return matchesFilter
+        })
+      }
     }
   }
 }
