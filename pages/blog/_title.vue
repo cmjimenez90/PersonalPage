1c480fa07501bef930aef06b099a79431cf519d0
Carlos Jimenez <developer@cmjimenez.com>
2019-04-10 16:05:43 -0400

    add rec default styling blog post buttercms

--- pages/blog/_title.vue
+++ pages/blog/_title.vue
@@ -23,5 +23,42 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus">
+.post-content
+    h1,h2,h3,h4,h5
+      font-weight: 600
+      margin-top: 1.5em
+      margin-bottom: 1em
+    ul, ol
+      margin-bottom: 1.25em
+      li
+        margin-bottom: 0.25em
+    p
+      font-size: 1.25em
+      line-height: 1.5
+      margin-bottom: 1.25em
+      font-weight: 400
+    img
+      max-width: 100%
+      height: auto
+    figcaption 
+      font-style: italic
+      text-align: center
+      color: #ccc
+    p code 
+      padding: 2px 4px
+      font-size: 90%
+      color: #c7254e
+      background-color: #f9f2f4
+      border-radius: 4px
+    pre
+      display: block
+      padding: 1em
+      margin: 0 0 2em
+      font-size: 1em
+      line-height: 1.4
+      word-break: break-all
+      word-wrap: break-word
+      color: #333333
+      background-color: #f5f5f5
 </style>
